c9e3e8ba9a552bcbbcf73b4887d6cc52524dbc68
JinwYu <jinwoojohanyu@gmail.com>
2018-04-06 10:42:45 +0200

    Implemented a second pitch detection method that uses autocorrelation because I'm not sure the current method is sufficient for human voices. Pitch shifting by a third party script is implemented and uses the 2^(1/12) method for each half step. I'm not sure this is the way to proceed; I might have to write a phase vocoder from scratch by myself. Before than at least, I will try to implement an algorithm that checks which note the fundamental frequency is closet to and scale to that frequency.

--- Assets/ImplementBernessScript.cs
+++ Assets/ImplementBernessScript.cs
@@ -1,28 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using B83.MathHelpers;
+using Pitch;
 
 public class ImplementBernessScript : MonoBehaviour {
 
     AudioSource audioSource;
+    static PitchTracker pitchTracker;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+
+        
+
+
 
         audioSource = GetComponent<AudioSource>();
 
         float[] tempSamples = new float[audioSource.clip.samples];
         audioSource.clip.GetData(tempSamples, 0);
+        //audioSource.Play();
 
-        PitchShifter.PitchShift(2.0f, tempSamples.LongLength, 44100, tempSamples);
+        pitchTracker = new PitchTracker
+        {
+            SampleRate = 44100.0,
+            RecordPitchRecords = true,
+            PitchRecordHistorySize = 10
+        };
+        pitchTracker.ProcessBuffer(tempSamples, tempSamples.Length); // Data to process.
+        //Debug.Log(pitchTracker.CurrentPitchRecord.Pitch);
+
+        //var pitchRecords = pitchTracker.PitchRecords;
+
+        //foreach (var pitchRecord in pitchRecords)
+        //{
+        //    //Debug.Log("Pitch skdajakdj = " + pitchRecord.Pitch.ToString());
+        //    // ... or whatever
+        //}
+
+        //for (int i = 0; i < pitchTracker.PitchRecords.Count; i++)
+        //{
+        //    Debug.Log("pitchjhadkjshakd = " );
+        //}
+        //Debug.Log(" AHAHAHAHA = " + test);
+
+
+        // Get initial pitch value.
+        DetectPitch(tempSamples);
+
+        double value = 2.0;
+        double power = 5.0 / 12.0;
+        double pitchFactor = System.Math.Pow(value, power);
+        //Debug.Log("factor = " + pitchFactor);
+        // (float pitchShift, long numSampsToProcess, long fftFrameSize, long osamp, float sampleRate, float[] indata)
+        //PitchShifter.PitchShift((float)pitchFactor, tempSamples.LongLength, 2048, 4, 44100, tempSamples);
         Debug.Log("Pitch shifted the clip.");
 
-        audioSource.clip = AudioClip.Create("pitch shifted clip", tempSamples.Length, audioSource.clip.channels, 44100, false);
-        audioSource.clip.SetData(tempSamples, 0);
-        audioSource.loop = true;
+        // Varför är det så kort klipp? är det för klassen returnerar en mindre version.
 
-        audioSource.Play();
+        //audioSource.clip = AudioClip.Create("pitch shifted clip", tempSamples.Length, audioSource.clip.channels, 44100, false);
+        //audioSource.clip.SetData(tempSamples, 0);
+        //audioSource.loop = true;
+
+        //audioSource.Play();
         Debug.Log("Playing pitchshifted clip.");
+
+        // Get the pitch value of the pitch shifted sound.
+        Debug.Log("Freq of pitch shifted sound: ");
+        DetectPitch(tempSamples);
+
+
+        // Det måste ju vara rätt pitch detection inne i pitchshifter scriptet
+        // eftersom den pitchar till rätt frekvens/ton. 
+        // Kan jag identifiera den koden som fixar det?
+        // För jag behöver veta fundamental frekvensen för att kunna bestämma vilken not jag ska
+        // pitcha till
+
+        //---------------------------------------------------------
+        // Gör en array med alla fundamentalfrekvenser 
+        // då kan jag få reda på generellt om ljudets frekvens är nära tex A eller B
+        // Räkna ut skillnaden i antal frekvenser som ljudet är fel i frekvens till den ton den är närmast tex A
+        // Konvertera denna differens till pitchfactor
+        // Pitcha enligt det
+
+        // eftersom algoritmen jag har någorlunda rätt kan identifiera vilken frekvens
+        // som ett ljud har, och eftersom det är multiplar till varje not, så räcker
+        // det bara att veta vilken not man vill pitchshifta till. Vilken oktav behövs därför inte
+        //-------------------------------------------------
+
+        // Bra svar här: https://stackoverflow.com/questions/1847633/net-library-to-identify-pitches
+        // MusiGenesis
+
 
         // Måste fixa hur 1 till 0.5 ger en oktav neråt och 1 till 2 ger en oktav uppåt
         // Måste reglera hur pitch factor ska reglera
@@ -35,5 +104,78 @@ public class ImplementBernessScript : MonoBehaviour {
         // det kommer uppstå konstiga skillnader i ton mellan klippen och förmodligen låta hemskt
 
         // Lära mig all teknik/matematik bakom den här skiten
+
+        // NEDAN ÄR BARA KOD FÖR ATT HITTA FREKVENSEN
+
+
     }
+
+
+
+    // Get fundamental frequency.
+    public void DetectPitch(float[] tempSamples)
+    {
+        int QSamples = 65536; //131072;
+        float[] tmp;
+        Complex[] spec2;
+        Complex[] spec3;
+
+        const float Threshold = 0.02f;
+        float PitchValue;
+        float _fSample;
+
+
+        tmp = new float[QSamples];
+        spec2 = new Complex[QSamples];
+        spec3 = new Complex[QSamples];
+
+        _fSample = AudioSettings.outputSampleRate;
+        //Debug.Log("_fsample = " + _fSample);
+
+        audioSource = GetComponent<AudioSource>();
+
+        float[] tempSamples1 = new float[audioSource.clip.samples];
+        audioSource.clip.GetData(tempSamples1, 0);
+
+        // copy the output data into the complex array
+        for (int i = 0; i < QSamples; i++) //tempSamples.Length
+        {
+            spec2[i] = new Complex(tempSamples[i], 0);
+            //Debug.Log("copying to complex");
+        }
+        // calculate the FFT
+        FFT.CalculateFFT(spec2, false);
+
+        float[] tempSamples2 = FFT.Complex2Float(spec2, false);
+
+        // Nu har vi en FFT:ad array, vi måste hitta frekvensen nu bara
+        float maxV = 0;
+        var maxN = 0;
+        for (int i = 0; i < QSamples; i++)
+        { // find max 
+            //if (!(tempSamples2[i] > maxV) || !(tempSamples2[i] > Threshold))
+            //    continue;
+            //Debug.Log("i = " + i);
+            if ((tempSamples2[i] > maxV) && (tempSamples2[i] > Threshold))
+            {
+                maxV = tempSamples2[i];
+                maxN = i; // maxN is the index of max
+                //Debug.Log("inside, i = " + i);
+            }
+        }
+
+        //Debug.Log("max index = " + maxN);
+        float freqN = maxN; // pass the index to a float variable
+        if (maxN > 0 && maxN < QSamples - 1)
+        { // interpolate index using neighbours
+            var dL = tempSamples2[maxN - 1] / tempSamples2[maxN];
+            var dR = tempSamples2[maxN + 1] / tempSamples2[maxN];
+            freqN += 0.5f * (dR * dR - dL * dL);
+        }
+        //PitchValue = freqN * (44100 / 2.0f) / QSamples; // convert index to frequency
+        PitchValue = freqN * 44100 / QSamples; // TODO: ändra till 48000
+        Debug.Log("pitch = " + PitchValue);
+    }
+
+
 }
---
+++ Assets/PhaseVocoder/PhaseVocoder.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseVocoder : MonoBehaviour {
+
+    int framesize = 2048;
+    int fftsize = 2048;
+    int analysis_hopsize = 256; // TODO: välj parametrar efter tabellen i rapporten
+    int resynthesis_hopsize;
+    int number_of_frame;
+    float[,] magnitude;
+    float[,] phase;
+    float[] buffer;
+    string window_type = "Hamming";
+    float[] windowbuffer;
+
+    AudioSource audioSource;
+
+    void Start () {
+
+        audioSource = GetComponent<AudioSource>();
+        float[] tempSamples = new float[audioSource.clip.samples];
+        audioSource.clip.GetData(tempSamples, 0);
+        int input_sound_size = tempSamples.Length;
+
+        number_of_frame = input_sound_size / analysis_hopsize;
+        magnitude = new float[number_of_frame, fftsize];
+        phase = new float[number_of_frame, fftsize];
+        buffer = new float[fftsize];
+        windowbuffer = new float[framesize];
+    }
+
+    /*
+        Input: input_sound_info, fftsize, window_type,
+        framesize, analysis_hopsize, number_of_frame
+        Output: magnitude, phase
+    */
+    /*
+        The analysis algorithm in the phase vocoder is follwing steps;
+        1. Take frame(N) size samples from input sound. Store them into a frame buffer.
+        2. Choose a proper window and window the buffer
+        3. Zero-pad the buffer
+        4. FFT-shift the zero-padded buffer.
+        5. Decompose the buffer by Fourier transform
+        6. Calculate and store magnitude and phase (output from analysis that is used in resynthesis).
+        7. Go back to step 1 and start taking frame size sample located at hop size distant from
+        the first sample of the previous loop.
+    */
+
+    private void Analysis(int input_sound_info, int fftsize, string window_type,
+    int framesize, int analysis_hopsize, int number_of_frame)
+    {
+        int input_index = 0;
+        float[] fft_complex_number = new float[fftsize];//has both real and imaginary parts
+
+        //windowbuffer = window(windowtype, framesize);
+        // TODO: fixa hur skapar ett window av hamming
+
+        for (int i = 0; i < number_of_frame; i++)
+        {
+            for (int j = 0; j < framesize; j++)
+            {
+                //buffer[j] = input_sound[input_index + j] * windowbuffer[j];
+            }
+            //fftshift(buffer);
+            //fft(buffer, fft_complex_number);
+            //get_magnitude(magnitude[i][], fft_complex_number);
+            //get_phase(phase[i][], fft_complex_number);
+            //input_index += analysis_hop;
+        }
+
+    }
+
+}
